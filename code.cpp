ba786b26042ac6290ed29b9bcce7c695d90ecc08
lto5 <79517614+lto5@users.noreply.github.com>
2021-05-04 22:44:42 +0700

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,19 @@
+/// For testing purpose
+#include <bits/stdc++.h>
+using namespace std;
+using iiii = pair<int,int>;
+// this is
+/// my code
+/* obfuscate by
+me */
+
+typedef long long ll;
+#define inf 1e9+7
+const ll m=11111000;
+
+int main(){
+  iiii test = {1,2};
+  ll infff=inf+m;
+  cout<<infff<<'\n';
+  return 0;
+}
